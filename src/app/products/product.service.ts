506a248f6aa34ad46f31c6e278260deab15eae8e
Luis Marquez <luis.marquez@e2ycommerce.com>
2024-10-07 09:47:22 +0200

    Added more methods on service

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -5,3 +5,20 @@ export const products: Product[] = [];
 export const addProduct = (data: Product): void => {
     products.push(data);
 };
+
+export const updateProduct = (id: string, data: Partial<Product>): void => {
+    const index = products.findIndex((product: Product) => product.id === id);
+    products[index] = {
+        ...products[index],
+        ...data,
+    }
+};
+
+export const deleteProduct = (id: string) => {
+    const index = products.findIndex((product: Product) => product.id === id);
+    products.splice(index, 1);
+}
+
+export const findProduct = (id: string): Product | undefined => {
+    return products.find((product: Product) => product.id === id);
+};
